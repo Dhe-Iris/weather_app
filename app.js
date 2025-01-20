d56eff8b9822af279ca15b4de14a4abf0dd0a8c1
Dhe-Iris <dolapog44@gmail.com>
2025-01-20 16:34:44 +0100

    integrated the Api

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+function updateCityTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.daily[0].temperature.day;
+
+  let cityName = document.querySelector("#weather-city");
+
+  cityName.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  console.log(temperatureElement);
+}
+
+function searchCity(city) {
+  // Fetch weather data for the provided city
+  let apiKey = "952c930fb329c422ctbo0af41a6cb25f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateCityTemperature);
+}
+
 function displayCityName(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityName = document.querySelector("#weather-city");
-  cityName.innerHTML = searchInput.value.trim();
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", displayCityName);
+
+searchCity("lagos"); // search lagos by default
